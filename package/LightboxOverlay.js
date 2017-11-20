731fa5498c606113b814a69d19f6c28cac06541a
Michael Andreas <michael@filtered.com>
2017-11-20 17:00:02 +0200

    let overlay can be zoomed.

--- package/LightboxOverlay.js
+++ package/LightboxOverlay.js
@@ -65,13 +65,11 @@ export default class LightboxOverlay extends Component {
     onClose:         PropTypes.func,
     willClose:         PropTypes.func,
     swipeToDismiss:  PropTypes.bool,
-    scalable: PropTypes.bool, // can be zoomed or not
   };
 
   static defaultProps = {
     springConfig: { tension: 30, friction: 7 },
     backgroundColor: 'black',
-    scalable: true,
   };
 
   state = {
@@ -84,106 +82,43 @@ export default class LightboxOverlay extends Component {
     },
     pan: new Animated.Value(0),
     openVal: new Animated.Value(0),
-    // for scalable
-    scale: 1,
-    lastScale: 1,
-    offsetX: 0,
-    offsetY: 0,
-    lastX: 0,
-    lastY: 0
-  };
-
-  distant = 150;
-  delay = 300;
-  radius = 20;
-  prevTouchInfo = {
-    prevTouchX: 0,
-    prevTouchY: 0,
-    prevTouchTimeStamp: 0,
   };
 
   componentWillMount() {
     this._panResponder = PanResponder.create({
       // Ask to be the responder:
-      onStartShouldSetPanResponder: (evt, gestureState) => {
-        return !this.state.isAnimating;
-      },
-      onMoveShouldSetPanResponder: (evt, gestureState) => {
-        if (this.state.isAnimating) {
-          return false;
-        } else {
-          return this.props.scalable && gestureState.dx > 2 || gestureState.dy > 2 || gestureState.numberActiveTouches === 2;
-        }
-      },
+      onStartShouldSetPanResponder: (evt, gestureState) => !this.state.isAnimating,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => !this.state.isAnimating,
+      onMoveShouldSetPanResponder: (evt, gestureState) => !this.state.isAnimating,
       onMoveShouldSetPanResponderCapture: (evt, gestureState) => !this.state.isAnimating,
+
       onPanResponderGrant: (evt, gestureState) => {
-        const currentTouchTimeStamp = Date.now();
         this.state.pan.setValue(0);
-        this.setState({
-          isPanning: true,
-        });
-        if ( this.isDoubleTap(currentTouchTimeStamp, gestureState) ) {
-          this.doubleTapZoom();
-        }
-        this.prevTouchInfo = {
-          prevTouchX: gestureState.x0,
-          prevTouchY: gestureState.y0,
-          prevTouchTimeStamp: currentTouchTimeStamp,
-        };
-        if (gestureState.numberActiveTouches === 2) {
-          this.distant = this.distance(evt.nativeEvent.touches[0].pageX, evt.nativeEvent.touches[0].pageY, evt.nativeEvent.touches[1].pageX, evt.nativeEvent.touches[1].pageY);
-        }
+        this.setState({ isPanning: true });
       },
-      onPanResponderMove: (evt, gestureState) => {
-        // zoom
-        if (gestureState.numberActiveTouches === 2) {
-          // let dx = Math.abs(evt.nativeEvent.touches[0].pageX - evt.nativeEvent.touches[1].pageX);
-          // let dy = Math.abs(evt.nativeEvent.touches[0].pageY - evt.nativeEvent.touches[1].pageY);
-          // let distant = Math.sqrt(dx * dx + dy * dy);
-
-          let distant = this.distance(evt.nativeEvent.touches[0].pageX, evt.nativeEvent.touches[0].pageY, evt.nativeEvent.touches[1].pageX, evt.nativeEvent.touches[1].pageY);
-          let scale = distant / this.distant * this.state.lastScale;
-          this.setState({ scale });
-        }
-        // translate
-        else if (gestureState.numberActiveTouches === 1 && this.state.scale > 1) {
-          let offsetX = this.state.lastX + gestureState.dx / this.state.scale;
-          let offsetY = this.state.lastY + gestureState.dy / this.state.scale;
-          this.setState({ offsetX, offsetY });
-        } else {
-          this.state.pan.setValue(gestureState.dy);
-        }
-      },
-
-      onPanResponderTerminationRequest: (evt, gestureState) => false,
+      onPanResponderMove: Animated.event([
+        null,
+        { dy: this.state.pan }
+      ]),
+      onPanResponderTerminationRequest: (evt, gestureState) => true,
       onPanResponderRelease: (evt, gestureState) => {
-        if (this.state.scale > 1 ){
+        if(Math.abs(gestureState.dy) > DRAG_DISMISS_THRESHOLD) {
           this.setState({
-            lastX: this.state.offsetX,
-            lastY: this.state.offsetY,
-            lastScale: this.state.scale
+            isPanning: false,
+            target: {
+              y: gestureState.dy,
+              x: gestureState.dx,
+              opacity: 1 - Math.abs(gestureState.dy / WINDOW_HEIGHT)
+            }
           });
+          this.close();
         } else {
-          this.resetOverlay();
-          if(Math.abs(gestureState.dy) > DRAG_DISMISS_THRESHOLD) {
-            this.setState({
-              isPanning: false,
-              target: {
-                y: gestureState.dy,
-                x: gestureState.dx,
-                opacity: 1 - Math.abs(gestureState.dy / WINDOW_HEIGHT)
-              }
-            });
-            this.close();
-          } else {
-            Animated.spring(
-              this.state.pan,
-              { toValue: 0, ...this.props.springConfig }
-            ).start(() => { this.setState({ isPanning: false }); });
-          }
+          Animated.spring(
+            this.state.pan,
+            { toValue: 0, ...this.props.springConfig }
+          ).start(() => { this.setState({ isPanning: false }); });
         }
       },
-      onShouldBlockNativeResponder: evt => false,
     });
   }
 
@@ -191,42 +126,6 @@ export default class LightboxOverlay extends Component {
     if(this.props.isOpen) {
       this.open();
     }
-  }
-
-  // calculate distance between presses
-  distance(x0, y0, x1, y1) {
-    return Math.sqrt( Math.pow(( x1 - x0 ), 2) + Math.pow(( y1 - y0 ), 2) );
-  }
-
-  // is double tap or not
-  isDoubleTap(currentTouchTimeStamp, {x0, y0}) {
-    const { prevTouchX, prevTouchY, prevTouchTimeStamp } = this.prevTouchInfo;
-    const dt = currentTouchTimeStamp - prevTouchTimeStamp;
-
-    return ( dt < this.delay && this.distance(prevTouchX, prevTouchY, x0, y0) < this.radius );
-  }
-
-  doubleTapZoom(){
-    if (this.state.scale !== 1) {
-      this.resetOverlay();
-    } else {
-      this.setState({
-        scale : 1.8,
-        lastScale : 1.8,
-      });
-    }
-  }
-
-  // reset children
-  resetOverlay(){
-    this.setState({
-      scale: 1,
-      lastScale: 1,
-      offsetX: 0,
-      offsetY: 0,
-      lastX: 0,
-      lastY: 0
-    });
   }
 
   open = () => {
@@ -268,13 +167,10 @@ export default class LightboxOverlay extends Component {
         isAnimating: false,
       });
       this.props.onClose();
-      // reset dispaly
-      this.resetOverlay();
     });
   }
 
   componentWillReceiveProps(props) {
-    // reset dispaly also can been called here
     if(this.props.isOpen != props.isOpen && props.isOpen) {
       this.open();
     }
@@ -285,7 +181,6 @@ export default class LightboxOverlay extends Component {
       isOpen,
       renderHeader,
       swipeToDismiss,
-      scalable,
       origin,
       backgroundColor,
     } = this.props;
@@ -302,7 +197,7 @@ export default class LightboxOverlay extends Component {
     };
 
     let handlers;
-    if(swipeToDismiss || scalable) {
+    if(swipeToDismiss) {
       handlers = this._panResponder.panHandlers;
     }
 
@@ -323,22 +218,15 @@ export default class LightboxOverlay extends Component {
 
     const background = (<Animated.View style={[styles.background, { backgroundColor: backgroundColor }, lightboxOpacityStyle]}></Animated.View>);
     const header = (<Animated.View style={[styles.header, lightboxOpacityStyle]}>{(renderHeader ?
-                                                                                   renderHeader(this.close) :
-                                                                                   (
-                                                                                     <TouchableOpacity onPress={this.close}>
-                                                                                       <Text style={styles.closeButton}>×</Text>
-                                                                                     </TouchableOpacity>
-                                                                                   )
+      renderHeader(this.close) :
+      (
+        <TouchableOpacity onPress={this.close}>
+          <Text style={styles.closeButton}>×</Text>
+        </TouchableOpacity>
+      )
     )}</Animated.View>);
     const content = (
-      <Animated.View style={[openStyle, dragStyle, {
-        transform: [
-          {scaleX: this.state.scale},
-          {scaleY: this.state.scale},
-          {translateX: this.state.offsetX},
-          {translateY: this.state.offsetY}
-        ]
-      }]} {...handlers}>
+      <Animated.View style={[openStyle, dragStyle]} {...handlers}>
         {this.props.children}
       </Animated.View>
     );
